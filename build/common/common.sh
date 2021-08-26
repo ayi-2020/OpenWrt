ab03bfa09fe1dd0a7576981f7430d74c5aee43e7
vie72 <69401701+vie72@users.noreply.github.com>
2021-08-26 22:50:02 +0800

    Update common.sh

--- build/common/common.sh
+++ build/common/common.sh
@@ -35,6 +35,10 @@ git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
 git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
 git clone --depth=1 https://github.com/garypang13/smartdns-le package/smartdns-le
+git clone https://github.com/kiddin9/luci-app-dnsfilter package/luci-app-dnsfilter
+git clone https://github.com/kenzok78/luci-theme-edge package/luci-theme-edge
+git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic package/luci-app-unblockneteasemusic
+
 
 sed -i "/exit 0/i\chmod +x /etc/webweb.sh && source /etc/webweb.sh" $ZZZ
 
